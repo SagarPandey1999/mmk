0446cdb0d1fc4cdfbb5279fa99f7f205c105d590
SagarPandey1999 <sgpd2709@gmail.com>
2021-08-25 17:01:51 +0530

    crud operation in company table and show company data frontend side

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Companys;
 use App\Models\contact;
 use Illuminate\Http\Request;
 
@@ -18,5 +20,53 @@ class AdminController extends Controller
         $data = contact::find($id);
         $data->delete();
         return redirect('contacts');
+    }
+
+    public function company(){
+        $data = Companys::all();
+        $container['companys'] = $data;
+        return view('admin.company.index',$container);
+    }
+
+    public function companycreate(){
+        return view('admin.company.create');
+    }
+
+    public function companyinsert(Request $request){
+        $data = new Companys();
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->description = $request->description;
+        $data->phone = $request->phone;
+        $data->title = $request->title;
+        $data->link = $request->link;
+        $data->save();
+        return redirect('company');
+    }
+
+    public function companydelete($id){
+        $data = Companys::find($id);
+        $data->delete();
+        return redirect('company');
+    }
+
+    public function companyedit($id){
+        $data = Companys::find($id);
+        $container['company'] = $data;
+        return view('admin.company.edit',$container);
+    }
+
+    public function companyupdate(Request $request){
+            $data = Companys::find($request->id);
+            if($data){
+                $data->title = $request->title;
+                $data->name = $request->name;
+                $data->email = $request->email;
+                $data->description = $request->description;
+                $data->phone = $request->phone;
+                $data->link = $request->link;
+                $data->save();
+                return redirect('company');
+            }
     }
 }
